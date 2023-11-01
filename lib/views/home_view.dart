64355ae6c1d64f1be8895adbbd8b72be473f4a85
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2023-11-01 21:33:22 +0200

    use asyn

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
 import 'package:news_app/widgets/news_list_view.dart';
+import 'package:gap/gap.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
