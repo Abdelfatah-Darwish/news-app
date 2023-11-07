6c162b04c0f5c25b0ec93f87aa51a883ec26637e
Abdelfatah-Darwish <abdelfatahdarwish13@gmail.com>
2023-11-07 16:06:40 +0200

    using cachNetorkimage

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -4,25 +4,57 @@ import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/new_services.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({
     super.key,
   });
 
   @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  late Future<List<ArticleModel>> future;
+  @override
+  void initState() {
+    super.initState();
+    future = NewServices(Dio()).getNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewServices(Dio()).getNews(),
+    return FutureBuilder<List<ArticleModel>>(
+        future: future,
         builder: (context, snapShot) {
-          return NewsListView(
-            articles: snapShot.data ?? [],
-          );
+          if (snapShot.hasData) {
+            return NewsListView(
+              articles: snapShot.data!,
+            );
+          } else if (snapShot.hasError) {
+            return const SliverToBoxAdapter(
+              child: ErrorMessage(
+                message: 'there are error,try later',
+              ),
+            );
+          } else {
+            return const SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
         });
-    // isLoading
-    //     ? const SliverToBoxAdapter(
-    //         child: Center(child: CircularProgressIndicator()))
-    //     : NewsListView(
-    //         articles: articles,
-    //       );
+  }
+}
+
+class ErrorMessage extends StatelessWidget {
+  const ErrorMessage({
+    super.key,
+    required this.message,
+  });
+  final String message;
+  @override
+  Widget build(BuildContext context) {
+    return Text(message);
   }
 }
